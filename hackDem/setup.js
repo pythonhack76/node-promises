2b0270ca917feac6e5a77df10ff0b9039bb1c9df
Python The Hack <lrulvoni@gmail.com>
2022-05-30 11:10:37 +0200

    update

---
+++ hackDem/setup.js
@@ -0,0 +1,12 @@
+let coordinate = [];
+let FirstEnemy = 'P';
+let enemies = [];
+let weapons = []; 
+let jokers = []; 
+
+
+function checkListEnemies() {
+    console.log(`nemico principale : ${FirstEnemy}` );
+}
+
+
